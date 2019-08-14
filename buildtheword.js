601f6a808bb468275e618883df31dc1782ece9f4
OxyCode <thegreatartem+git@gmail.com>
2019-08-15 00:20:53 +0800

    added difficuly for buildtheword

--- buildtheword.js
+++ buildtheword.js
@@ -1,6 +1,5 @@
 
 
-
 extras = ['fire']
 let LOADED_IMAGES = new ImageLoader('../images/', extras);
 let loaded = false;
@@ -8,7 +7,7 @@ let loaded = false;
 let background = 'bg' + (Math.random() * 8 | 0).toString() + '.jpg';
 document.body.style.backgroundColor = 'grey'
 document.body.style.backgroundImage = 'url(../images/' + background.toString() + ')';
-document.body.style.backgroundSize = width + 'px auto';
+//document.body.style.backgroundSize = width + 'px auto';
 document.body.style.backgroundRepeat = 'no-repeat';
 
 
@@ -16,12 +15,11 @@ let chosen = words[Math.random() * words.length | 0];
 
 let letters = chosen.split('');
 
-let modes = ['firesandmonster', 'twomonsters', 'chasing'];
-let chosen_mode = Math.random() * 3 | 0;
+let modes = ['fires', 'monsters', 'ghosts'];
+let chosen_mode = Math.random() * 4| 0;
 if (chosen_mode === 3) {
     chosen_mode--;
 }
-
 let ps = [];
 let promises = letters.map((letter, i) => {
     let p = new Promise(resolve => {
@@ -31,8 +29,10 @@ let promises = letters.map((letter, i) => {
                 par = new P(letter, Math.random() * (width - 100), Math.random() * 100);
                 par.set('color', 'white');
                 par.set('text-shadow', 'black 2px 2px 2px')
-                par.set('font-size', '5em');
+                par.set('font-size',( width<400? 3: 5 )+'em');
                 par.set('z-index', 5);
+                par.set('padding',0)
+                par.set('margin',0)
                 par.shape.addEventListener('mousedown', (ev) => {
                     if (!par.locked) {
                         dropAll()
@@ -67,7 +67,7 @@ let promises = letters.map((letter, i) => {
 let lines = [];
 Promise.all(promises).then(res => {
     ps = res;
-    let w = 50; //line width
+    let w = width<400? 30:50; //line width
     let space = 5; //space width
     let y_calc = height - 50;
     let num = ps.length;
@@ -90,7 +90,8 @@ Promise.all(promises).then(res => {
     lines = lines.filter(x => Object.keys(x).length);
     setTimeout(() => {
         generateObstacles(modes[chosen_mode]);
-    }, 3000)
+        start()
+    }, 2000)
 
 });
 
@@ -105,7 +106,7 @@ let dragging_offset = new Vector(0, 0);
 function drag(ev) {
     ps.forEach(x => {
         if (x.dragging) {
-            x.set('top', (ev.clientY - x.shape.offsetHeight * 1.5) + 'px');
+            x.set('top', (ev.clientY - x.shape.offsetHeight/2) + 'px');
             x.set('left', (ev.clientX - x.shape.offsetWidth / 2) + 'px');
         }
     })
@@ -114,7 +115,7 @@ function drag(ev) {
 function pickup(letter, ev) {
     if (!letter.locked && !letter.resetting) {
         letter.set('color', 'blue');
-        letter.set('top', (ev.clientY - letter.shape.offsetHeight * 1.5) + 'px');
+        letter.set('top', (ev.clientY - letter.shape.offsetHeight/2) + 'px');
         letter.set('left', (ev.clientX - letter.shape.offsetWidth / 2) + 'px');
         letter.dragging = true;
     }
@@ -152,22 +153,24 @@ let powers = ['iceball', 'electricball', 'magicball', 'fire', 'waterball', 'blac
 
 function generateObstacles(mode) {
     switch (mode) {
-        case 'firesandmonster':
+        case modes[0]:
             let firenum = (Math.random() * (difficulty * 2 + 1) | 0) + 5;
             if (Math.random() < (0.4 + difficulty / 10)) {
-                monsters.push(new Character(800, 300, 'monster' + (Math.random() * 30 | 0)));
+                monsters.push(new Character(width*Math.random(), 300, 'monster' + (Math.random() * 30 | 0)));
                 monsters[0].bounds.y = height - 150;
-                monsters[0].addSprite(new Img('../images/' + monsters[0].name + '.png', 800, 300, 200));
+                monsters[0].bounds.x = width
+                monsters[0].addSprite(new Img('../images/' + monsters[0].name + '.png', monsters[0].x, 300, 200));
                 requestAnimationFrame(() => {
                     monsters[0].sprite.shape.addEventListener('click', () => {
                         let n = Math.random()
-                        if (n > .7) {
+                        monsters[0].a.add(new Vector(n*40-20,-30))
+                       /* if (n > .7) {
                             monsters[0].jumpleft();
                         } else if (n > .3) {
                             monsters[0].jump()
                         } else {
                             monsters[0].jumpright();
-                        }
+                        }*/
                     })
                 })
             } else {
@@ -175,17 +178,15 @@ function generateObstacles(mode) {
             }
 
             for (let i = 0; i < firenum; i++) {
-                let xx = Math.random() * 100 + (850 / firenum) * i + (Math.random() * 40 - 20);
+                let xx = Math.random() * width*0.05 + (width*.8 / firenum) * i + (Math.random() * 40 - 20);
                 let yy = Math.random() * 300;
-                if (xx > 300 && xx < 650 && yy > 250) {
-                    yy -= 200;
+                if (xx > width*.3 && xx < width*.7 && yy > height*.7) {
+                    yy -= height*.4;
                 }
                 fires.push(new PowerBall(xx, yy, powers[Math.random() * powers.length | 0]));
             }
             fires.forEach(fire => {
                 fire.addSprite(new Img('../images/' + fire.name + '.png', 100, 100, 50));
-                fire.bounds = {x: 10000, y: 10000}
-                fire.noskybox = true;
                 fire.nobounds = true;
             });
 
@@ -197,39 +198,41 @@ function generateObstacles(mode) {
                 });
             });
             break;
-        case 'twomonsters':
+        case modes[1]:
             let num = (Math.random() * (difficulty + 1.1) | 0) + 2;
 
             for (let i = 0; i < num; i++) {
 
-                let mon = new Character(200 + (600 / num) * i, 300, 'monster' + (Math.random() * 30 | 0));
-                mon.addSprite(new Img('../images/' + mon.name + '.png', 800, 300, 200))
+                let mon = new Character(width*.1 + ((width*.8)/ num) * i, 300, 'monster' + (Math.random() * 30 | 0));
+                mon.addSprite(new Img('../images/' + mon.name + '.png', mon.x, 300, 200))
                 requestAnimationFrame(() => {
                     mon.sprite.shape.addEventListener('click', () => {
                         let n = Math.random()
+                        mon.a.add(new Vector(n*40-20,-30))
+                        /*
                         if (n > .7) {
                             mon.jumpleft();
                         } else if (n > .3) {
                             mon.jump()
                         } else {
                             mon.jumpright();
-                        }
+                        }*/
                     })
                 })
-
+                mon.bounds.x = width
                 mon.bounds.y = height - 100;
                 monsters.push(mon)
 
             }
             break;
-        case 'chasing':
+        case modes[2]:
 
             let chasenum = (Math.random() * (difficulty * 3) | 0) + 4;
             for (let i = 0; i < chasenum; i++) {
-                let xx = Math.random() * 100 + (Math.random() * (950 / chasenum) + 80) * i;
-                let yy = Math.random() * 300 + 50;
-                if (xx > 300 && xx < 650 && yy > 250) {
-                    yy -= 200;
+                let xx = Math.random() * width*0.05 + (width*.8 / chasenum) * i + (Math.random() * 40 - 20);
+                let yy = Math.random() * 300;
+                if (xx > width*.3 && xx < width*.7 && yy > height*.7) {
+                yy -= height*.4;
                 }
                 fires.push(new Character(xx, yy, 'ghost' + i % 11));
             }
@@ -238,31 +241,50 @@ function generateObstacles(mode) {
                 fire.bounds = {x: width, y: height}
                 fire.antigrav = true;
                 fire.max_v = Math.random() * 7 + 9 * difficulty;
-                fire.max_f = Math.random() * (difficulty + .6) / (9 - (difficulty ));
+                fire.max_f = Math.random() * (difficulty*1.1 + .6) / (9 - (difficulty ));
             });
 
 
         default:
     }
-    start();
+    id('jmpleft').innerText = diff2word(difficulty)
+    
+    id('jump').innerText = modes[chosen_mode]
+    
     loaded = true;
 }
 
 function winningCleanup(mode) {
     switch (mode) {
-        case 'firesandmonster':
-        case 'chasing':
+        case modes[0]:
+        case modes[2]:
             fires.forEach(f => {
                 f.kill();
             })
-        case 'twomonsters':
+        case modes[1]:
             monsters.forEach(m => {
                 m.kill();
             })
             stop();
     }
 }
-
+function cleanup(mode) {
+    switch (mode) {
+        case modes[0]:
+        case modes[2]:
+            fires.forEach(f => {
+                f.sprite.destroy()
+                f.rect.destroy()
+            })
+            fires = [];
+        case modes[1]:
+            monsters.forEach(m => {
+                m.sprite.destroy()
+                m.rect.destroy()
+            })
+            monsters = []
+    }
+}
 function reset(letter) {
     requestAnimationFrame(() => {
         if (!letter.locked) {
@@ -286,7 +308,7 @@ loop = function (now) {
     let dt = now-LAST_TIME;
     fires.forEach(fire => {
         fire.update(dt);
-        if (modes[chosen_mode] === 'chasing') {
+        if (chosen_mode === 2) {//if chasing
             ps.forEach(p => {
                 if (p.dragging) {
                     let x = parseInt(p.shape.offsetLeft + p.shape.offsetWidth / 2);
@@ -348,11 +370,13 @@ loop = function (now) {
                 })
                 lines.forEach(line => {
                     if (line.target === letter.string) {
-                        if (line.a.copy().add(new Vector(25, -25)).dist(new Vector(x, y)) < 10) {
-
+                        if (line.a.copy().add(new Vector(line.length/2, line.length/-2)).dist(new Vector(x, y)) < line.length/3) {
+                            jlog(line)
                             letter.locked = true;
                             letter.set('color', 'green');
                             letter.set('z-index', 0);
+                            letter.set('top',line.a.y -sh.offsetHeight + 5+'px')
+                            letter.set('left',line.a.x + line.length/2 - sh.offsetWidth/2+ 'px')
                             letter.dragging = false;
                             line.target += 'done';
                         }
@@ -371,3 +395,25 @@ loop = function (now) {
         requestAnimationFrame(loop)
     }
 };
+
+id('jmpleft').addEventListener('click',()=>{
+     if(loaded){
+     difficulty++
+     difficulty%=4
+     id('jmpleft').innerText = diff2word(difficulty)
+     cleanup(modes[chosen_mode])
+     generateObstacles(modes[chosen_mode])
+     }
+   
+})
+id('jump').addEventListener('click',()=>{
+    if(loaded){
+    cleanup(modes[chosen_mode])
+    
+    chosen_mode++
+    chosen_mode%=modes.length
+    id('jump').innerText = modes[chosen_mode]
+    
+    generateObstacles(modes[chosen_mode])
+    }
+})
