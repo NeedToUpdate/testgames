5c390afdf63e996c8be389a06945d83a17e59801
Artem <thegreatartem+git@gmail.com>
2019-10-22 16:38:51 +0800

    started halloween animation

---
+++ images/people/zzz_fix_images.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+
+dir = os.getcwd() + '\\'
+
+for file in os.listdir(dir):
+	if(file):
+		print(dir+file);
+		img = cv2.imread(dir + file, -1)
+		new_img = cv2.flip(img, 1)
+		#cv2.imshow(file,img)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+		print(file)
+		cv2.imwrite(file, new_img)
