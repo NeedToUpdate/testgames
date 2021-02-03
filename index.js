1a71b1fa868aab7f1a5f950e6da1559d27870309
needtoupdate <updatethispl0x@gmail.com>
2021-02-03 10:54:12 +0800

    pwa small fix

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ if( "serviceWorker" in navigator){
     navigator.serviceWorker.register("./sw.js").then(registration=>{
         console.log("registered!",registration)
     }).catch(e=>{
-        console.log("failed reg.",e)
+        console.log("failed reg.")
+        console.log(e)
     })
 }else{
 
